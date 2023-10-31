fea809c7eceea376bfe4c3213c75e0b98d9f6b53
juaninsau9 <juaninsau9@gmail.com>
2023-10-30 21:22:43 -0300

    se agregaron a la base de datos precios y stock

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
-import { Router } from "express"; // Importa la clase Router de Express, que permite definir rutas.
-import { libro } from "./controller.js"; // Importa el objeto 'libro' desde el archivo 'controller.js'.
+import { Router } from "express";
+import { libro } from "./controller.js";
 
-export const router = Router(); // Crea una nueva instancia de Router y la asigna a la variable 'router'.
+export const router = Router();
 
-router.get('/libros', libro.getAll); // Ruta HTTP GET, que llama al método getAll en el objeto libro, para obtener todos los libros.
-router.post('/libros', libro.add); // Ruta HTTP POST, que llama al método add en el objeto libro, para agregar un libro.
-router.delete('/libros/:id', libro.deleteId); // Ruta HTTP DELETE, que llama al método deleteId en el objeto libro, para eliminar un libro a través de su id.
-router.delete('/libros/ISBN/:ISBN', libro.deleteISBN); // Ruta HTTP DELETE, que llama al método deleteISBN en el objeto libro, para eliminar un libro a través de su ISBN.
-router.put('/libros/actualizar/:id', libro.update); // Ruta HTTP PUT, que llama al método update en el objeto libro, para actualizar un libro.
-router.get('/libros/:id', libro.getOne); // Ruta HTTP GET, que llama al método getOne en el objeto libro, para obtener un libro a través su ID.
+router.get('/libros', libro.getAll);
+
+router.post('/libros', libro.add);
+
+router.delete('/libros/:id', libro.deleteId);
+
+router.delete('/libros/ISBN/:ISBN', libro.deleteISBN);
+
+router.put('/libros/actualizar/:id', libro.update);
+
+router.get('/libros/:id', libro.getOne);
