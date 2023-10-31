b78b317233b0f537399c0dc72c8d6f29f87b265d
juaninsau9 <juaninsau9@gmail.com>
2023-10-30 21:26:40 -0300

    se hizo la eliminacion de un libro por ISBN

--- src/controller.js
+++ src/controller.js
@@ -15,11 +15,9 @@ class LibroController {
         try {
             const libroId = req.params.id;
             const [result] = await pool.query('SELECT * FROM libros WHERE id = ?', [libroId]);
-
             if (result.length === 0) {
                 return res.status(404).json({ error: 'Libro no encontrado' });
             }
-
             res.json(result[0]);
         } catch (error) {
             console.error(error);
@@ -30,11 +28,9 @@ class LibroController {
     async add(req, res) {
         try {
             const libro = req.body;
-            const { nombre, autor, categoria, año_publicacion, ISBN, precio, stock } = libro;
-            
+            const { nombre, autor, categoria, año_publicacion, ISBN, precio, stock } = libro;  
             const query = "INSERT INTO libros(nombre, autor, categoria, año_publicacion, ISBN, precio, stock) VALUES (?, ?, ?, ?, ?, ?, ?)";
-            const values = [nombre, autor, categoria, año_publicacion, ISBN, precio, stock];
-            
+            const values = [nombre, autor, categoria, año_publicacion, ISBN, precio, stock]; 
             const [result] = await pool.query(query, values);
             res.json({ "Id insertado": result.insertId });
         } catch (error) {
@@ -59,11 +55,9 @@ class LibroController {
 
     async deleteISBN(req, res) {
         const libroISBN = req.params.ISBN;
-
         if (!/^\d{13}$/.test(libroISBN)) {
             return res.status(400).json({ error: 'Número de ISBN incorrecto. Debe contener 13 dígitos.' });
         }
-
         try {
             const query = "DELETE FROM libros WHERE ISBN = ?";
             const values = [libroISBN];
@@ -79,7 +73,7 @@ class LibroController {
             res.status(500).json({ error: 'Error al eliminar un libro por ISBN' });
         }
     }   
-    
+
     async update(req, res) {
         try {
             const libro = req.body;
@@ -88,16 +82,12 @@ class LibroController {
             if (!/^\d{13}$/.test(ISBN)) {
                 return res.status(400).json({ error: 'Número de ISBN incorrecto. Debe contener 13 dígitos.' });
             }
-
             const query = "UPDATE libros SET nombre=?, autor=?, categoria=?, año_publicacion=?, ISBN=?, precio=?, stock=? WHERE id=?";
             const values = [nombre, autor, categoria, año_publicacion, ISBN, precio, stock, id];
-
             const [result] = await pool.query(query, values);
-
             if (result.changedRows === 0) {
                 return res.status(404).json({ error: 'Libro con este ID no encontrado en la base de datos' });
             }
-
             res.json({ "Registros actualizados": result.changedRows });
         } catch (error) {
             console.error(error);
@@ -105,5 +95,4 @@ class LibroController {
         }
     } 
 }
-
 export const libro = new LibroController();
